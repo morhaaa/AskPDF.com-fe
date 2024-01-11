a3931545e1c9b26a402727c017967a241307eab8
Mohamed <mohamed.rhanmi@gmail.com>
2024-01-11 10:41:08 +0100

    Add toaster provider

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,12 +1,18 @@
 import { store } from "@/containers/store";
 import React, { ReactNode } from "react";
+import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
 
 type ProvidersProps = {
   children: ReactNode;
 };
 function Providers({ children }: ProvidersProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <Toaster />
+      {children}
+    </Provider>
+  );
 }
 
 export default Providers;
