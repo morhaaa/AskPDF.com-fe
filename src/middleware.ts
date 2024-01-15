4d43e43268b18552bc8de5dedf7593efeed7cb3a
Mohamed <mohamed.rhanmi@gmail.com>
2024-01-15 16:29:12 +0100

    feat: implement cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
-import { isAuthenticated } from "@/utils/auth";
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/dashboard"];
 
-export default function middleware(req: NextRequest) {
-  if (!isAuthenticated && protectedRoutes.includes(req.nextUrl.pathname)) {
+export default async function middleware(req: NextRequest) {
+  const cookie = req.cookies.get("jwt_token");
+
+  if (!cookie && protectedRoutes.includes(req.nextUrl.pathname)) {
     const absoluteURL = new URL("/", req.nextUrl.origin);
+    return NextResponse.redirect(absoluteURL.toString());
+  }
+
+  if (cookie && req.nextUrl.pathname.includes("/login" && "/sign-in")) {
+    const absoluteURL = new URL("/dashboard", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
 }
