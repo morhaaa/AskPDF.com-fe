f0aa3f10a1dc27bd1198fc5ca0e94816719e0d55
mohamed rhanmi <106605846+morhaaa@users.noreply.github.com>
2024-01-16 08:07:12 +0100

    fix: fix redirect bug

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+'use client'
 import React from "react";
+import axios from "axios";
+import SideMenu from "@/components/side-menu";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+
+
+  return <div className="h-screen w-screen flex justify-center items-center">
+      <SideMenu/>
+  </div>;
 }
 
 export default Dashboard;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 const protectedRoutes = ["/dashboard"];
@@ -11,7 +10,7 @@ export default async function middleware(req: NextRequest) {
     return NextResponse.redirect(absoluteURL.toString());
   }
 
-  if (cookie && req.nextUrl.pathname.includes("/login" || "/sign-up")) {
+  if (cookie && req.nextUrl.pathname.includes("/login") || req.nextUrl.pathname.includes("/sign-up")) {
     const absoluteURL = new URL("/dashboard", req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
