d48368d4073eb9d4941d38b93459940ccdc71184
Mohamed <mohamed.rhanmi@gmail.com>
2024-01-22 17:27:18 +0100

    feat: add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
 import Features from "@/components/features";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center gap-y-8">
+      <Navbar />
       <Intro />
       <Features />
     </main>
