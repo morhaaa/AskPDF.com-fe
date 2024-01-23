53361e2c3fec1b70cbdb63485f0ebea3146c1ac9
Mohamed <mohamed.rhanmi@gmail.com>
2024-01-23 07:43:29 +0100

    feat: Add link to button

--- src/components/return-home.tsx
+++ src/components/return-home.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 
 function ReturnToHomePage() {
   return (
     <div className="flex items-center gap-2">
-      <button className="bg-gray-300/60 rounded-full h-10 w-10 border drop-shadow-2xl flex items-center justify-center cursor-pointer hover:scale-105">
+      <Link
+        href="/"
+        className="bg-gray-300/60 rounded-full h-10 w-10 border drop-shadow-2xl flex items-center justify-center cursor-pointer hover:scale-105"
+      >
         <ArrowLeft strokeWidth={2.4} />
-      </button>
+      </Link>
       <span>AskPDF</span>
     </div>
   );
