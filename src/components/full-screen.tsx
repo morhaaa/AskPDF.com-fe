7c95a87294c58f983efcbf9fd5b372372d9a8a2c
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-02-07 14:59:20 +0100

    new: create fullscreen component

---
+++ src/components/full-screen.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { Expand, Loader2 } from "lucide-react";
+import SimpleBar from "simplebar-react";
+import { Page, Document, pdfjs } from "react-pdf";
+import useWindowWidth from "@/hooks/useWidth";
+import toastError from "@/utils/toast-error";
+
+interface FullScreenProps {
+  fileUrl: string | undefined;
+}
+
+const FullScreen: React.FC<FullScreenProps> = ({ fileUrl }) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [numPages, setNumPages] = useState<number>(0); // Total pdf pages
+
+  const documentRef = useRef(null);
+  const documentWidth = useWindowWidth(documentRef);
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(visible) => {
+        if (!visible) {
+          setIsOpen(visible);
+        }
+      }}
+    >
+      <Button onClick={() => setIsOpen(true)} variant={"ghost"}>
+        <Expand className="h-4 w-4" />
+      </Button>
+      <DialogContent
+        close={() => {
+          setIsOpen(false);
+        }}
+        className="max-w-7xl w-full"
+      >
+        <SimpleBar autoHide={false} className="h-[calc(100vh-6rem)] mt-6">
+          <div ref={documentRef}>
+            <Document
+              file={fileUrl}
+              loading={
+                <div className="flex justify-center items-center ">
+                  <Loader2 className="my-40 h-6 w-6 animate-spin" />
+                </div>
+              }
+              onLoadSuccess={({ numPages }) => {
+                setNumPages(numPages);
+              }}
+              onLoadError={() => {
+                toastError("Error loading PDF, Please try again");
+              }}
+              className="max-h-full"
+            >
+              {new Array(numPages).fill(0).map((_, i) => (
+                <Page
+                  key={i}
+                  width={documentWidth ? documentWidth : 1}
+                  pageNumber={i + 1}
+                />
+              ))}
+            </Document>
+          </div>
+        </SimpleBar>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default FullScreen;
