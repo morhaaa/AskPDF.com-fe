65d85d32a4ad3d3f9dd1bc6ea03e41719aa81c4e
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-02-13 08:34:12 +0100

    feat: implement chat input component

--- src/components/chat/chat-input.tsx
+++ src/components/chat/chat-input.tsx
@@ -1,36 +1,52 @@
-import React, { MouseEventHandler, useState } from "react";
-import { ChevronUpCircle, Send, SendHorizonal } from "lucide-react";
+import React, { useContext } from "react";
+import { ChevronUpCircle } from "lucide-react";
 import { Button } from "../ui/button";
+import TextareaAutosize from "react-textarea-autosize";
+import { ChatContext } from "./chat-context";
 
 interface ChatInputProps {
-  disabled?: boolean;
+  isDisabled?: boolean;
 }
-const ChatInput: React.FC<ChatInputProps> = ({ disabled }) => {
-  const [message, setMessage] = useState<string>("");
+const ChatInput: React.FC<ChatInputProps> = ({ isDisabled }) => {
+  const { message, handleMessage, isLoading, addMessage } =
+    useContext(ChatContext);
 
-  const onSubmit = (message: string) => {};
-  const handleMessageSubmit = (e: React.FormEvent) => {
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (message.trim() !== "") {
-      onSubmit(message);
-      setMessage("");
+      console.log(message);
+      addMessage();
     }
+  };
+
+  const handleTextArea = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+    handleMessage(value);
   };
 
   return (
     <div className="p-4 bg-gradient-to-t from-zinc-100 to-zinc-50 ">
       <form
-        onSubmit={handleMessageSubmit}
+        onSubmit={onSubmit}
         className="flex items-center justify-between bg-white shadow-md rounded-lg px-2 py-2"
       >
-        <input
-          type="text"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
+        <TextareaAutosize
+          //value={message}
+          onChange={handleTextArea}
+          minRows={1}
+          maxRows={5}
           placeholder="Type your message..."
           className="flex-1 ml-3 focus:outline-none text-sm"
+          style={{
+            resize: "none",
+          }}
         />
-        <Button type="submit" variant="ghost" disabled className="px-3">
+        <Button
+          type="submit"
+          variant="ghost"
+          disabled={isLoading || isDisabled}
+          className="px-3"
+        >
           <ChevronUpCircle
             strokeWidth={"2.5px"}
             className="h-4 w-4 text-slate-700"
