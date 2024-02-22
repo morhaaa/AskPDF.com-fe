26561c4c38983f30a6ece0d241c4ecf3028e183f
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-02-22 18:15:12 +0100

    fix: change redirect middleware from sign-up to login

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    experimental: {
-      appDir: true,
-      esmExternals: "loose", 
-    },
-    webpack: (config) => {
-      config.externals = [...config.externals, { canvas: "canvas" }]; 
-      return config;
-    },
-  };
+  reactStrictMode: false,
+  experimental: {
+    appDir: true,
+    esmExternals: "loose",
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }];
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
