f559c9c34bcf3e6262e3d6d5d23774dd16403f00
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-02-27 18:18:28 +0100

    chore: add small feautre

--- src/components/upload-button.tsx
+++ src/components/upload-button.tsx
@@ -1,30 +1,34 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import DND from "./drag-n-drop"
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import DND from "./drag-n-drop";
 
 type Props = {
   triggerButton: () => void;
+  disabled: boolean;
 };
 
-const UploadButton: React.FC<Props> = ({triggerButton}) => {
+const UploadButton: React.FC<Props> = ({ triggerButton, disabled }) => {
   return (
-      <Dialog>
-        <DialogTrigger className="bg-blue-600 text-sm xl:text-base text-white font-semibold py-2 px-3 xl:px-4 rounded-sm font-sans hover:bg-blue-500">
-            Upload PDF
-        </DialogTrigger>
-        <DialogContent close={triggerButton}>
-            <DialogHeader>
-            <DialogTitle>Upload you file here</DialogTitle>
-            </DialogHeader>
-            <DND/>
-        </DialogContent>
+    <Dialog>
+      <DialogTrigger
+        disabled={disabled}
+        className="bg-blue-600 text-sm xl:text-base text-white font-semibold py-2 px-3 xl:px-4 rounded-sm font-sans hover:bg-blue-500 disabled:bg-zinc-200"
+      >
+        Upload PDF
+      </DialogTrigger>
+      <DialogContent close={triggerButton}>
+        <DialogHeader>
+          <DialogTitle>Upload you file here</DialogTitle>
+        </DialogHeader>
+        <DND />
+      </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
-export default UploadButton
+export default UploadButton;
