427e7eb3f92cae8c38bc6e8927fe5b145ab5b7c6
MohaCodeploy <mohamed.rhanmi@gmail.com>
2024-02-27 18:17:52 +0100

    chore: add small feature useAuth

--- src/hooks/userAuth.ts
+++ src/hooks/userAuth.ts
@@ -1,15 +1,17 @@
 "use client";
 import { setCredentials } from "@/containers/authSlice";
 import { StoreType } from "@/containers/store";
-import toastError from "@/utils/toast-error";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import toastError from "@/utils/toast-error";
+import { getCookies, setCookie, deleteCookie, getCookie } from "cookies-next";
 
 const url = `${process.env.NEXT_PUBLIC_BE_URL}/v1/auth`;
 
 const useAuth = () => {
   const [userInfo, setUserInfo] = useState<IUser | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   //Redux
   const user = useSelector((store: StoreType) => store.user.data);
@@ -17,8 +19,11 @@ const useAuth = () => {
 
   useEffect(() => {
     if (user) {
+      setIsLoading(true);
       setUserInfo(user);
+      setIsLoading(false);
     } else {
+      setIsLoading(true);
       const authUser = async () => {
         try {
           const result = await axios.get(url, {
@@ -35,16 +40,21 @@ const useAuth = () => {
             };
             setUserInfo(info);
             dispatch(setCredentials(info));
+          } else {
+            setUserInfo(null);
+            dispatch(setCredentials(null));
           }
         } catch (error) {
           toastError(`Error: ${error}`);
+        } finally {
+          setIsLoading(false);
         }
       };
       authUser();
     }
-  }, [dispatch, user]);
+  }, [dispatch]);
 
-  return userInfo;
+  return { userInfo, isLoading };
 };
 
 export default useAuth;
